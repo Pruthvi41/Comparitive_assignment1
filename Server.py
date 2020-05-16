97c2c1dc2b027c78d4cf53771d28fc4e831fe55c
pruthvi raju <Pruthvi41>
2020-05-15 23:16:39 -0400

    Dictonary added

--- Server.py
+++ Server.py
@@ -6,4 +6,12 @@ s.listen(1)
 while True:
     clientsocket, address = s.accept()
     print(f"connection from {address} has connected!")
-    clientsocket.send(bytes("hi","utf-8"))
+    input_File= open("demo.txt",'r')
+    dict={}
+    for line in input_File:
+        name = line.split("|")
+        dict = {name[0]: (name[1], name[2], name[3])}
+        print(dict)
+
+
+    clientsocket.send(bytes("gvh","utf-8"))

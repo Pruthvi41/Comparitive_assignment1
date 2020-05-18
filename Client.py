57dc27abe728707cbd4ac91a716c7b2868680e21
pruthvi raju <Pruthvi41>
2020-05-18 00:46:14 -0400

    Two way communication

--- Client.py
+++ Client.py
@@ -1,6 +1,32 @@
-import socket
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(),9999))
+import socket,time,sys
+print("Initialising....\n")
+time.sleep(1)
 
-msg=s.recv(1024)
-print(msg.decode("utf-8"))
+s = socket.socket()
+shost = socket.gethostname()
+ip = socket.gethostbyname(shost)
+print(shost, "(", ip, ")\n")
+host = input(str("Enter server address: "))
+port = 9999
+print("\nTrying to connect to ", host, "(", port, ")\n")
+time.sleep(1)
+s.connect((host, port))
+print("Connected...\n")
+
+#s.send(name.encode())
+#s_name = s.recv(1024)
+#s_name = s_name.decode()
+
+while True:
+    print("Python DB Menu\n1. Find customer\n2. Add customer\n3. Delete customer\n4. Update customer age\n5. Update customer address\n6. Update customer phone\n7. Print report\n8. Exit")
+    select_Option=input("Select: ")
+    s.send(bytes(select_Option,"utf-8"))
+    name=s.recv(1024)
+    name=name.decode("utf-8")
+    print(name)            
+
+            
+    #s.send(message.encode())
+
+
+    
--- Server.py
+++ Server.py
@@ -1,16 +1,33 @@
-import socket
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(),9999))
+import time, socket, sys
+print("Initialising....\n")
+time.sleep(1)
+
+s = socket.socket()
+host = socket.gethostname()
+ip = socket.gethostbyname(host)
+port = 9999
+s.bind((host, port))
+print(host, "(", ip, ")\n")           
 s.listen(1)
+print("\nWaiting for incoming connections...\n")
+conn, addr = s.accept()
+print("Received connection from ", addr[0], "(", addr[1], ")\n")
+input_File= open("demo.txt",'r')
+dict={}
+for line in input_File:
+    name = line.split("|")
+    dict[name[0]]=(name[1:])
+
+#conn.send(name.encode())
+
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"connection from {address} has connected!")
-    input_File= open("demo.txt",'r')
-    dict={}
-    for line in input_File:
-        name = line.split("|")
-        dict[name[0]]=(name[1:])
-    print(dict)
-
-    clientsocket.send(bytes("gvh","utf-8"))
+    s_name = conn.recv(1024)
+    s_name = s_name.decode("utf-8")
+    print(s_name)
+    conn.send(bytes("Hello","utf-8"))
+    #conn.send(bytes("gvh","utf-8"))
+    #conn.send(message.encode())
+    #message = conn.recv(1024)
+    #message = message.decode()
+    #print(s_name, ":", message)
